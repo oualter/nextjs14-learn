648ef286c2cc7f84d95a3959d62fa09ac752130c
Olivier Prêtre <o.pretre.ext@funecap.com>
2023-11-15 14:30:10 +0100

    til chapter 12

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -16,12 +16,12 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
     </Link>
-  );
+  )
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
